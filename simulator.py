1737d787fb4d8c8ffe9f5835923a8ef2f7550bae
Max Maass <max@velcommuta.de>
2016-02-08 11:52:41 +0100

    Update simulator with friend logic and impr. stats
    Friend relationships are now explicitly tracked and symmetric.
    Stats are now displayed as whitespace-separated values.

--- simulator.py
+++ simulator.py
@@ -7,6 +7,7 @@ import simpy
 import random
 
 SIMULATION_ROUNDS = 361
+SIMULATION_ITERATIONS = 10
 INITIAL_USERS = 100
 
 
@@ -19,10 +20,12 @@ class User(object):
         self.env = env
         # The process that performs the simulation work
         self.action = env.process(self.run())
-        # Number of friends
-        self.friends = 0
+        # Friends
+        self.friends = set([])
         # Number of pieces of data shared
         self.shares = 0
+        # Number of share operations performed
+        self.share_ops = 0
         # Last share happened at timestep
         self.last_share = 0
         # Status of the user - False means user has stopped using the system
@@ -31,11 +34,16 @@ class User(object):
         self.joined = env.now
         # Simulation step in which the user has left the system
         self.left = -1
+        # Received, dowloaded shares
+        self.shares_downloaded = 0
+        # Received, but not yet downloaded shares
+        self.shares_received = 0
 
     def run(self):
         """Main simulation loop."""
         while self.active:
             self.make_friends()
+            self.retrieve_data()
             self.share_data()
             self.quit_application()
             yield self.env.timeout(1)
@@ -43,59 +51,106 @@ class User(object):
     def make_friends(self):
         """Make new friends, with a certain probability."""
         if random.random() < 0.3:
-            self.friends += 1
+            newfriend = random.sample(self.env.active_users - self.friends, 1)[0]
+            self.friends.add(newfriend)
+            newfriend.friends.add(self)
+
+    def retrieve_data(self):
+        """Retrieve incoming shares, with a certain probability."""
+        if random.random() < 0.5:
+            self.shares_downloaded += self.shares_received
+            self.shares_received = 0
 
     def share_data(self):
         """Share data with all friends, with a certain probability."""
         if random.random() < 0.1 + (self.env.now - self.last_share) * 0.1:
-            self.shares += 1 + self.friends
+            # If data is shared, received shares are also retrieved
+            self.shares_downloaded += self.shares_received
+            self.shares_received = 0
+            # Incr. the number of sent shares to 1 plus the number of friends,
+            # as we are uploading one data block and n key blocks
+            self.shares += 1 + len(self.friends)
+            self.share_ops += 1
+            # Set the timestamp of the last sent share to now
             self.last_share = self.env.now
+            # For each friend, increment the number of received shares
+            for friend in self.friends:
+                friend.shares_received += 1
 
     def quit_application(self):
         """Quit using the application, with a certain probability."""
         if random.random() < 0.01:
             self.active = False
             self.left = self.env.now
-            self.env.inactive_users.append(self)
+            self.env.inactive_users.add(self)
             self.env.active_users.remove(self)
 
 
-env = simpy.Environment()
-env.active_users = [User(env) for i in range(INITIAL_USERS)]
-env.inactive_users = []
+def run(stdout=False):
+    """Main simulation loop."""
+    env = simpy.Environment()
+    env.active_users = set([User(env) for i in range(INITIAL_USERS)])
+    env.inactive_users = set([])
 
+    results = {}
 
-while env.peek() < SIMULATION_ROUNDS:
-    last = env.now != env.peek()
-    if last:
-        # The next step will be the first in the next simulation timestep
-        if env.peek() % 10 == 0:
-            # Ten steps have passed, gather data
-            print "Step", env.now + 1
-            active = len(env.active_users)
-            inactive = len(env.inactive_users)
-            shares = 0
-            friends = 0
-            for user in env.active_users:
-                shares += user.shares
-                friends += user.friends
-            print "  Active:  ", active
-            print "  Inactive:", inactive
-            print "  Shares:  ", shares
-            print "  Friends: ", friends
-    env.step()
-    if last:
-        env.active_users += [User(env) for i in range(random.randrange(0, 5, 1))]
+    while env.peek() < SIMULATION_ROUNDS:
+        last = env.now != env.peek()
+        if last:
+            # The next step will be the first in the next simulation timestep
+            if env.peek() % 10 == 0:
+                # Ten steps have passed, gather statistics
+                active = len(env.active_users)
+                inactive = len(env.inactive_users)
+                shares = 0
+                friends = 0
+                nodownload = 0
+                inac_friends = 0
+                share_ops = 0
+                # Iterate over all users, active and inactive
+                for user in env.active_users | env.inactive_users:
+                    shares += user.shares
+                    share_ops += user.share_ops
+                    friends += len(user.friends)
+                    nodownload = nodownload + user.shares - user.shares_downloaded
+                    inac_friends += len(user.friends & env.inactive_users)
+                # Print stats
+                if stdout:
+                    print "Step", env.now + 1
+                    print "  Users:      ", active + inactive
+                    print "    Active:   ", active
+                    print "    Inactive: ", inactive
+                    print "  Shares:     ", shares
+                    print "    Share ops:", share_ops
+                    print "    Not retr: ", nodownload
+                    print "  Friends:    ", friends
+                    print "    Inactive: ", inac_friends
+                else:
+                    results[env.now + 1] = {
+                        "users": active + inactive,
+                        "users_active": active,
+                        "users_inactive": inactive,
+                        "shares": shares,
+                        "share_ops": share_ops,
+                        "share_noretr": nodownload,
+                        "friends": friends,
+                        "friends_inactive": inac_friends
+                    }
+        env.step()
+        if last:
+            # Add new users to the system
+            env.active_users.update([User(env) for i in range(random.randrange(0, 5, 1))])
+    return results
 
+res = {}
+avg = {}
+for i in range(SIMULATION_ITERATIONS):
+    res[i] = run()
 
-# Complex model: More realistic, but a lot more work
-# - Users joining
-#   With which distribution?
-# - Users leaving
-#   With which distribution?
-# - Users becoming friends
-#   Achieve power-law distribution in friend count
-# - Users sharing data
-#   Between once per Month and once per day, peaking at three times a week
-# - Users retrieving data
-#   When?
+print "iteration round users active inactive shares shareops nodownload friends inacfriends"
+for i in range(SIMULATION_ITERATIONS):
+    for k in sorted(res[i].keys()):
+        print i, k, res[i][k]["users"], res[i][k]["users_active"], \
+            res[i][k]["users_inactive"], res[i][k]["shares"], \
+            res[i][k]["share_ops"], res[i][k]["share_noretr"], \
+            res[i][k]["friends"], res[i][k]["friends_inactive"]
